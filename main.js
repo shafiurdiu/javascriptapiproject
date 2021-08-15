81814487821539c7deb1fc4f1d97f53ff2926f28
Md Ikbal Razu <37942730+ikbalrazu@users.noreply.github.com>
2021-08-15 18:11:30 +0600

    added loader on fetching data from rest api

--- main.js
+++ main.js
@@ -1,63 +1,91 @@
 
+var loader = document.getElementById("loading");
+var infocard = document.getElementById("infocard");
+
+function hide(){
+    loader.style.visibility = "hidden";
+    infocard.style.visibility = "hidden";
+
+}
+
+
 
 function searchresult(){
-    
+    loader.style.visibility = "visible";
     confiq = {
         method: "get",
         url: "https://jsonplaceholder.typicode.com/users",
     };
     var promiseobj = axios(confiq);
     promiseobj.then((res) => {
+        return res.data;
 
-        var getdata = res.data;
-        console.log(getdata);
-        searchbox(getdata);
-
-    });
-    
+    }).then(searchbox)
+    .catch(err => {
+      console.log('Error: ', err);
+    })
 }
 
-function searchbox(receivedata){
+function searchbox(data){
+    loader.style.visibility = "hidden";
     var input, filter1;
     input = document.getElementById("searchBox");
-    filter1 = input.value;
-    console.log(filter1);    
-    if(filter1.length <= 1){
-
-        for(var i=0; i<receivedata.length; i++){
-            var resultlist = document.getElementById("results");
-    
-            var item_area = document.createElement("div");
-            item_area.className = "item";
-    
-            resultlist.appendChild(item_area);
-    
-            item_area.setAttribute("id","myselect");
-    
-            var content_area = document.createElement("div");
-            content_area.className = "content";
-            item_area.appendChild(content_area);
-    
-             
-    
-            var header_area = document.createElement("div");
-            content_area.appendChild(header_area);
-            resultlist.setAttribute("style", "display: block !important;");
-            header_area.innerText = receivedata[i].name;
-            header_area.setAttribute("id",receivedata[i].id);
-            header_area.setAttribute("onclick","selectuser(this.id)");
+    var resultlist = document.getElementById("results");
+    if (resultlist.hasChildNodes()){
+        console.log("Yes");
+        for (let i=0; resultlist.childNodes.length; i++){
+            resultlist.removeChild(resultlist.childNodes[i]);
         }
+    }
+    filter1 = input.value;
 
+    if (filter1.length == 0){
+        infocard.style.visibility = "hidden";
+    }
+
+    if (filter1.length >= 1){
+        for(var i=0; i<data.length; i++){
+            fname = data[i].name.toLowerCase();
+            console.log(data[i].name, filter1, data[i].name.includes(filter1));
+            if (fname.includes(filter1.toLowerCase())){
+                
+    
+                var item_area = document.createElement("div");
+                item_area.className = "item";
+        
+                resultlist.appendChild(item_area);
+        
+                item_area.setAttribute("id","myselect");
+        
+                var content_area = document.createElement("div");
+                content_area.className = "content";
+                item_area.appendChild(content_area);
+
+                var header_area = document.createElement("div");
+                content_area.appendChild(header_area);
+                resultlist.setAttribute("style", "display: block !important;");
+                header_area.innerText = data[i].name;
+                header_area.setAttribute("id",data[i].id);
+                header_area.setAttribute("onclick","selectuser(this.id)");
+            }
+        }
     }
 
 }
 
 function selectuser(selectID){
-    console.log(selectID);
 
-    let name = document.querySelector("#name");
-    let username = document.querySelector("#username");
-    let email = document.querySelector("#email");
+    
+
+    infocard.style.visibility  = "visible";
+    loader.style.visibility = "visible";
+
+
+    let pinfo = document.getElementById("pinfo");
+    let phone = document.getElementById("phone");
+    let address = document.getElementById("address");
+    let company = document.getElementById("company");
+    let companylink = document.getElementById("companylink");
 
     config = {
         method: "get",
@@ -65,11 +93,14 @@ function selectuser(selectID){
     };
     const promiseobj = axios(config);
     promiseobj.then((res) => {
+        loader.style.visibility = "hidden";
         let singleData = res.data;
-        console.log(singleData);
         // createList(alldata);
-        name.value = singleData.name;
-        username.value = singleData.username;
-        email.value = singleData.email;
+        pinfo.innerHTML = "Full Name: " + singleData.name + "<br>"+ "Username: " +singleData.username + "<br>" + "Email: " +singleData.email;
+        phone.innerHTML = "Phone: " + singleData.phone;
+        company.innerHTML = "Company: " + singleData.company.name;
+        companylink.href = "https://" + singleData.website;
+        companylink.innerHTML = singleData.website;
+        address.innerHTML = "Address: " + singleData.address.city + ", "+ singleData.address.street + ", "+ singleData.address.zipcode;
     });
 }
